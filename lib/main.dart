f5313060eb7d462f3d6136270a6f64f8540e4f99
Hoaveth <hoaveth.gaming@gmail.com>
2023-06-27 10:48:07 +0800

    Added a navigation rail and layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,60 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          body: Row(
+        children: [
+          SafeArea(
+              child: NavigationRail(
+            extended: constraints.maxWidth >= 600,
+            destinations: [
+              NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text("Home")),
+              NavigationRailDestination(
+                  icon: Icon(Icons.favorite), label: Text("Favorites")),
+            ],
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+          )),
+          Expanded(
+            child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page),
+          )
+        ],
+      ));
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -58,32 +111,29 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          WordCard(word: word),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                  icon: Icon(icon),
-                  onPressed: () {
-                    appState.addFavoriteWord();
-
-                    Future.delayed(Duration(milliseconds: 600), () {
-                      appState.generateWord();
-                    });
-                  },
-                  label: Text("Like")),
-              SizedBox(width: 20),
-              ElevatedButton(
-                  onPressed: () => appState.generateWord(),
-                  child: Text("Generate"))
-            ],
-          ),
-        ]),
-      ),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        WordCard(word: word),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                icon: Icon(icon),
+                onPressed: () {
+                  appState.addFavoriteWord();
+                  Future.delayed(Duration(milliseconds: 600), () {
+                    appState.generateWord();
+                  });
+                },
+                label: Text("Like")),
+            SizedBox(width: 20),
+            ElevatedButton(
+                onPressed: () => appState.generateWord(),
+                child: Text("Generate"))
+          ],
+        ),
+      ]),
     );
   }
 }
