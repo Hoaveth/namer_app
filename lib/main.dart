a0faa72c566e6a20c8178730c40fcf6825d5417f
Hoaveth <hoaveth.gaming@gmail.com>
2023-06-27 09:39:14 +0800

    Updated theme colors and UI display

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: 'Namer App',
           theme: ThemeData(
               useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
           home: MyHomePage(),
         ));
   }
@@ -36,14 +36,50 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var word = appState.currentWord;
 
     return Scaffold(
-      body: Column(children: [
-        Text('A random AWESOME name:'),
-        Text(appState.currentWord.asUpperCase),
-        ElevatedButton(
-            onPressed: () => appState.generateWord(), child: Text("Generate"))
-      ]),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          WordCard(word: word),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () => appState.generateWord(), child: Text("Generate"))
+        ]),
+      ),
+    );
+  }
+}
+
+class WordCard extends StatelessWidget {
+  const WordCard({
+    super.key,
+    required this.word,
+  });
+
+  final WordPair word;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary, shadows: [
+      Shadow(
+          color: Colors.black.withOpacity(0.3),
+          offset: const Offset(5, 5),
+          blurRadius: 5),
+    ]);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          word.asLowerCase,
+          style: style,
+          semanticsLabel: "${word.first} ${word.second}",
+        ),
+      ),
     );
   }
 }
